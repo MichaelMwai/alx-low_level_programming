68c702a33a5b6cfe221800e8e0cd0f6e7a6c575b
Michael Mwai <michaeldmwai@gmail.com>
2022-09-09 02:32:30 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  *main - entry point
- *return: 0
+ *
+ * Return: 0 after printing the function
  */
 int main(void)
-{
-	puts("\"Programming is like buildiing a multilingual puzzle");
+{puts("\"Programming is like buildiing a multilingual puzzle");
 	return (0);
 }
