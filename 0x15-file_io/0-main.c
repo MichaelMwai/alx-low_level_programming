afa8139202c25a91514aa1f105d386273a5942b4
Michael Mwai <michaeldmwai@gmail.com>
2024-09-19 12:46:29 +0300

    textfile.

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -5,20 +5,34 @@
 /**
  * main - check the code
  *
+ * @ac: Argument count
+ * @av: Argument vector
+ *
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
     ssize_t n;
+    char *buffer;
 
     if (ac != 2)
     {
         dprintf(2, "Usage: %s filename\n", av[0]);
         exit(1);
     }
+
+    /* Create a buffer using the create_buffer function */
+    buffer = create_buffer(av[1]);
+    
+    /* Read and print the file */
     n = read_textfile(av[1], 114);
     printf("\n(printed chars: %li)\n", n);
+    
     n = read_textfile(av[1], 1024);
     printf("\n(printed chars: %li)\n", n);
+
+    /* Free the allocated buffer */
+    free(buffer);
+
     return (0);
 }
--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,32 @@
-#include "main.h"
-#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
- */
+  * read_textfile - Reads a text file and prints it to the standard output
+  * @filename: The name of the file to read
+  * @letters: Number of letters it should read and print
+  *
+  * Return: Number of letters it could print
+  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	int fd, check, len = 0;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
 
-	free(buf);
+	check = read(fd, buff, letters);
+	if (check == -1)
+		return (0);
+
+	len = write(STDOUT_FILENO, buff, check);
 	close(fd);
-	return (w);
+	return (len);
 }
